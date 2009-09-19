260617043a59e95e2dc12dd5833c99efadfd1403
KKLOLK <KKLOLK@d1d97c42-6e42-11de-b5d2-8dea7b56f9cf>
2009-09-19 03:14:59 +0000

    backup before arraylist changes

--- trunk/drkuo/uonetwork.cs
+++ trunk/drkuo/uonetwork.cs
@@ -7,21 +7,30 @@ using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Collections;
+
+// C# to convert a byte array to a string.
+//byte [] dBytes = ...
+//string str;
+//System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+//str = enc.GetString(dBytes);
 
 namespace drkuo
 {
     class uonetwork
     {
+        private byte[] key = new byte[4];
+        private Boolean bDecompress = false;
         public String myoutput = "";
         public string myvars = "";
         public uoplayer player = new uoplayer();
         public uoobject drawdata; // rename?
-        public UOopcodes opcode;
+        //public UOopcodes opcode = new UOopcodes();
 
         private Socket mysocket;
-        private StateObject mystate;
+        public StateObject mystate;
 
-        HuffmanDecompression decomp;
+        HuffmanDecompression decomp = new HuffmanDecompression();
         private String ip;
         private int port;
         private String user;
@@ -44,7 +53,7 @@ namespace drkuo
         public void main()
         {
             mystate = new StateObject();
-            mystate.buffer = new byte[mystate.size];
+            //mystate.buffer = new byte[mystate.size];
             // Entry point for network thread
             Connect();
             Thread.Sleep(500);
@@ -53,8 +62,11 @@ namespace drkuo
             {
                 if (mysocket.Available > 0)
                 {
-                    display("data avail");
-                    mysocket.BeginReceive(mystate.buffer, 0, mystate.size, SocketFlags.None, PacketReceived, mystate);
+                    mystate.buffer = new byte[mysocket.Available];
+                    mystate.decomp = new byte[mysocket.Available];
+                    mystate.avail = mysocket.Available;
+                    //display("data avail");
+                    mysocket.BeginReceive(mystate.buffer, 0, mysocket.Available, SocketFlags.None, PacketReceived, mystate);
                     //mysocket.Receive(mystate.buffer);
                     
 
@@ -68,17 +80,94 @@ namespace drkuo
         {
             mysocket.EndReceive(result);
             StateObject newstate = (StateObject)result.AsyncState;
+            ArrayList incomingpackets = new ArrayList();
+            incomingpackets.AddRange(newstate.buffer);
+            byte[] outbuffer;
+            int refsize = 0;
+            while (newstate.avail > 0)
+            {
+                if (bDecompress)
+                {
+                    decomp.DecompressOnePacket(ref newstate.buffer, newstate.buffer.Length, ref newstate.decomp, ref refsize);
+                    // store incoming packet in an arraylist
+                    // decompress it etc, then xfer to byte[] and remove from list
+                    // repeat
+                    outbuffer = newstate.decomp;
+                }
+                else
+                {
+                    outbuffer = newstate.buffer;
+                }
+
+                handlePackets(outbuffer);
+            }
+        }
+
+        private void handlePackets(byte[] packetinfo)
+        {
+            
             try
             {
-                string cmd = Convert.ToString(newstate.buffer[0]);
-                display("Received >> " + BitConverter.ToString(newstate.buffer));
+                int cmd = Convert.ToInt32(packetinfo[0]);
+                string cmd2 = Convert.ToString(packetinfo[0]);
+                display("Received >> " + BitConverter.ToString(packetinfo));
+                switch(cmd)
+                {
+                    case UOopcodes.SMSG_GameServerList://0xA8:
+                        handleGameServerList();
+                        break;
+                    case UOopcodes.SMSG_ConnectToGameServer: // decomp after this
+                        handleConnectToGameServer(packetinfo);
+                        bDecompress = true;
+                        break;
+                    default:
+                        Console.WriteLine("Default case");
+                        break;
+                }
+
             }
             
             catch
             { }
-
         }
 
+        private void handleConnectToGameServer(byte[] buffer)
+        {
+                    display("Handling Connect to Game Server");
+        key[0] = buffer[7];
+        key[1] = buffer[8];
+        key[2] = buffer[9];
+        key[3] = buffer[10];
+        GameServerLogin();
+        }
+
+        private void GameServerLogin()
+        {
+            byte[] buffer = new byte[65];
+            buffer[0] = 0x91;
+            buffer[1] = key[0];
+            buffer[2] = key[1];
+            buffer[3] = key[2];
+            buffer[4] = key[3];
+            byte[] myuser = GetBytes(user);
+            byte[] mypass = GetBytes(pass);
+
+            for(int i = 0;i < (myuser.Length);i++)
+            {
+                buffer[i + 5] = myuser[i];
+            }
+            for (int i = 0; i < (mypass.Length); i++)
+            {
+                buffer[i + 35] = mypass[i];
+            }
+             Send(buffer);
+        }
+        private void handleGameServerList()
+        {
+            display("Handling GameServerList");
+            byte[] mybyte = {0xA0, 0x0, 0x0};
+            Send(mybyte);
+        }
         public void Login()
         {
             byte[] firstbyte = { 0xf, 0x70, 0x0, 0x1 };
@@ -100,7 +189,11 @@ namespace drkuo
             Send(loginpack);
             //Sends user/pass and FE as the login key, can use any key
         }
-
+        
+        public static byte[] GetBytes(string text)
+        {
+            return ASCIIEncoding.UTF8.GetBytes(text);
+        }
         public void Send(byte[] buffer)
         {
         //    StateObject newstate = new StateObject();
@@ -115,23 +208,14 @@ namespace drkuo
             mysocket.EndSend(AR);
         }
 
-        public string PacketToHex(byte[] buffer)
-        {
-            string mystring = "";
-            for (int i = 0; i <= (buffer.Length - 1); i++)
-            {
-                if (i > 80)
-                {
-                    return mystring;
-                }
-                mystring = mystring + Convert.ToString(buffer[i]) + " ";
-            }
-            return mystring;
-        }
-
+        
         public void display(String temp)
         {
             myoutput = myoutput + "\r\n" + temp;
+        }
+        public void updatevars()
+        {
+            myvars = myvars + "omg vars :o";
         }
         public void Connect()
         {    
@@ -157,7 +241,7 @@ namespace drkuo
        public int size = 4096;
        public byte[] buffer;
        public byte[] decomp;
-        
+       public int avail;
     }
 
 }
