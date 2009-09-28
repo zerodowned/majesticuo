196b48b6d620cad2965c2d8c5126c5f7454cc8f6
KKLOLK <KKLOLK@d1d97c42-6e42-11de-b5d2-8dea7b56f9cf>
2009-09-28 12:43:06 +0000

    Exposed uonet data to the Script class in which a script can be written and ran. Moved sent packets out of the network class.

---
+++ trunk/drkuo/Script.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace drkuo
+{
+    class Script
+    {
+        uonetwork uonet;
+        public Script(uonetwork muonet)
+        {
+            uonet = muonet;
+        }
+
+        public void main()
+        {
+            uonet.display("Script Ran");
+        }
+    }
+}
