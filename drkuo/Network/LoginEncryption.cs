98c2e71943e5766522fd2906d222961d1d09b172
kklolk <kklolk@d1d97c42-6e42-11de-b5d2-8dea7b56f9cf>
2010-03-25 13:27:46 +0000

    missed some files from last commit

---
+++ drkuo/Network/LoginEncryption.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+// CLient sends 0a010103 then sends 0x80 crypted
+//Sent >>0A-01-01-03
+//PreCrypt: 80-63-61-6C-69-73-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-6A-75-6E-6B-32-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-00-FE
+//Sent >>20-CC-C9-C7-43-99-0A-7A-C2-1E-70-C7-1C-71-C7-9C-31-E7-8C-B9-A3-2E-E8-0B-7A-42-DE-90-37-64-CD-F3-C6-48-87-3B-7B-42-DE-90-37-E4-0D-F9-03-FE-80-BF-A0-AF-A8-AB-2A-EA-0A-7A-C2-1E-70-C7-1C-8F
+//Received >> 82-04
+
+
+namespace drkuo.Network
+{
+    class LoginEncryption
+    {
+        // Client 3.0.0x Keys
+        public uint CurrentKey0;
+        public uint CurrentKey1;
+        public uint backup0;
+        public uint backup1;
+        public static uint FirstClientKey = 0x2D93A5FD;//289686529;//0x2d93a5fd;
+        public static uint SecondClientKey = 0xA3DD527F;//2747883135;//0xa3dd527f;
+        public uint EncryptionSeed;
+
+        public void init(uint Seedbyte)
+        {
+            EncryptionSeed = Seedbyte;
+            CurrentKey0 = (uint)((((~EncryptionSeed) ^ 0x00001357) << 16) | ((EncryptionSeed ^ 0xFFFFAAAA) & 0x0000FFFF));
+            CurrentKey1 = (uint)(((EncryptionSeed ^ 0x43210000) >> 16) | (((~EncryptionSeed) ^ 0xABCDFFFF) & 0xFFFF0000));
+            backup0 = CurrentKey0;
+            backup1 = CurrentKey1;
+        }
+        public byte[] Encrypt(byte[] data)
+        {
+            int len = data.Length;
+          for(int i = 0; i < len; i++)
+          {
+              // Decrypt the byte:
+              data[i] = (byte)(CurrentKey0 ^ data[i]);
+              
+              // Reset the keys:
+              uint oldkey0 = CurrentKey0;
+              uint oldkey1 = CurrentKey1;
+              CurrentKey0 = (uint)(((oldkey0 >> 1) | (oldkey1 << 31)) ^ SecondClientKey);
+              CurrentKey1 = (uint)((((((oldkey1 >> 1) | (oldkey0 << 31)) ^ (FirstClientKey - 1)) >> 1) | (oldkey0 << 31)) ^ FirstClientKey);
+              
+          }
+          CurrentKey0 = backup0;
+          CurrentKey1 = backup1;
+          return data;
+        }
+        public byte[] clientDecrypt(byte[] buffer)
+        {
+            uint eax, ecx, edx, esi;
+            int len = buffer.Length;
+            for (int i = 0; i < len; i++)
+            {
+                buffer[i] = (byte)(buffer[i] ^ (byte)(CurrentKey0 & 0xFF));
+                edx = CurrentKey1;
+                esi = CurrentKey0 << 31;
+                eax = CurrentKey1 >> 1;
+                eax |= esi;
+                eax ^= FirstClientKey - 1;
+                edx <<= 31;
+                eax >>= 1;
+                ecx = CurrentKey0 >> 1;
+                eax |= esi;
+                ecx |= edx;
+                eax ^= FirstClientKey;
+                ecx ^= SecondClientKey;
+                CurrentKey0 = ecx;
+                CurrentKey1 = eax;
+            }
+            return buffer;
+        }
+
+        public byte[] Encrypt2(byte[] clear)
+        {
+            uint table0 = 0, table1 = 0;
+            byte[] encrypted = new byte[clear.Length];
+
+            for (int i = 0; i < clear.Length; i++)
+            {
+                encrypted[i] = (byte)((clear[i]) ^ ((byte)CurrentKey0));
+
+                table0 = CurrentKey0;
+                table1 = CurrentKey1;
+
+                CurrentKey0 = ((table0 >> 1) | (table1 << 31)) ^ SecondClientKey;
+                table1 = ((table1 >> 1) | (table0 << 31)) ^ FirstClientKey;
+                CurrentKey1 = ((table1 >> 1) | (table0 << 31)) ^ FirstClientKey;
+            }
+
+            return encrypted;
+        } 
+
+    }
+}
---
+++ drkuo/Network/TwoFish.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Encryption
+{
+    class TwoFish
+    {
+private Encryption.TwofishEncryption engine;
+
+		private ushort recvPos; // Position in our CipherTable (Recv)
+		private byte sendPos; // Offset in our XOR Table (Send)
+		private byte[] cipherTable;
+		private byte[] xorData; // This table is used for encrypting the server->client stream
+
+		public void GameEncryption(uint seed) 
+		{
+			cipherTable = new byte[0x100];
+	            			
+			// Set up the crypt key
+			byte[] key = new byte[16];
+			key[0] = key[4] = key[8] = key[12] = (byte)((seed >> 24) & 0xff);
+			key[1] = key[5] = key[9] = key[13] = (byte)((seed >> 16) & 0xff);
+			key[2] = key[6] = key[10] = key[14] = (byte)((seed >> 8) & 0xff);
+			key[3] = key[7] = key[11] = key[15] = (byte)(seed & 0xff);
+
+			byte[] iv = new byte[0];
+			engine = new Encryption.TwofishEncryption(128, ref key, ref iv, CipherMode.ECB, Encryption.TwofishBase.EncryptionDirection.Encrypting);
+
+			// Initialize table
+			for ( int i = 0; i < 256; ++i )
+				cipherTable[i] = (byte)i;
+
+			sendPos = 0;
+
+			// We need to fill the table initially to calculate the MD5 hash of it
+			refreshCipherTable();
+
+			// Create a MD5 hash of the twofish crypt data and use it as a 16-byte xor table
+			// for encrypting the server->client stream.
+			MD5 md5 = new MD5CryptoServiceProvider();
+			xorData = md5.ComputeHash(cipherTable);
+		}
+
+		private void refreshCipherTable() 
+		{
+			uint[] block = new uint[4];
+
+			for (int i = 0; i < 256; i += 16)
+			{			
+				Buffer.BlockCopy(cipherTable, i, block, 0, 16);
+				engine.blockEncrypt(ref block);
+				Buffer.BlockCopy(block, 0, cipherTable, i, 16);
+			}
+
+			recvPos = 0;
+		}
+	        
+		public void serverEncrypt(ref byte[] buffer, int length) 
+		{
+			byte[] packet = new byte[length];
+			for ( int i = 0; i < length; ++i ) 
+			{
+				packet[i] = (byte)(buffer[i] ^ xorData[sendPos++]);
+				sendPos &= 0x0F; // Maximum Value is 0xF = 15, then 0xF + 1 = 0 again
+			}
+			buffer = packet;
+		}
+
+        public void clientEncryptData(ref byte[] buffer, int length) 
+		{
+			for (int i = 0; i < length; ++i) 
+			{
+				// Recalculate table
+				if ( recvPos >= 0x100 )
+				{
+					byte[] tmpBuffer = new byte[0x100];
+					refreshCipherTable();
+				}
+
+				// Simple XOR operation
+				buffer[i] ^= cipherTable[recvPos++];
+			}
+		}
+	}
+}
